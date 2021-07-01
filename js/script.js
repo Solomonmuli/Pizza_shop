61ddbb0da1ddbed36033894a7940c1137c813810
solo <solomon.munyalo@student.moringaschool.com>
2021-07-01 14:44:47 +0300

    Define variable & add the pop up info

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const closeBtn = document.querySelectorAll('.fa-times-circle');
+const cartList = document.querySelector(".pop-op");
+const menuList = document.querySelectorAll(".odr");
+const price = document.querySelector('#price');
+const totalPayment = document.querySelector(".pymnt");
+const quantity = document.querySelector(".quantity");
+
